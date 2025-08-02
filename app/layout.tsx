dbc67701498d28a1e5d12aa527d51944d3add468
jubautistap <jubautistap.zz.io.vn>
2025-08-02 16:03:11 +0300

    Добавлен счетчик Яндекс.Метрики

--- app/layout.tsx
+++ app/layout.tsx
@@ -208,6 +208,28 @@ export default function RootLayout({
           />
         </AmoCRMProvider>
         
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103567092', 'ym');
+
+              ym(103567092, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103567092" style={{position:'absolute', left:'-9999px'}} alt="" />
+          </div>
+        </noscript>
+        
         {/* Загружаем остальные схемы асинхронно */}
         <script
           type="application/ld+json"
