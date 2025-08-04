e1248523217b1aa886a180437d3aa8467e64a367
jubautistap <jubautistap.zz.io.vn>
2025-08-04 13:45:38 +0300

    🔧 Исправил Яндекс.Метрику: убрал обёртку в window.load, добавил диагностику, восстановил стандартную инициализацию

--- app/layout.tsx
+++ app/layout.tsx
@@ -238,22 +238,24 @@ export default function RootLayout({
           />
         </AmoCRMProvider>
         
-        {/* Yandex.Metrika counter - асинхронная загрузка для оптимизации критического пути */}
+        {/* Yandex.Metrika counter - оптимизированная загрузка */}
         <script
           type="text/javascript"
-          async
           dangerouslySetInnerHTML={{
             __html: `
-              window.addEventListener('load', function() {
-                (function(m,e,t,r,i,k,a){
-                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();
-                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103567092', 'ym');
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103567092', 'ym');
 
-                ym(103567092, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
-              });
+              ym(103567092, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+              
+              // Диагностика для проверки работы метрики
+              if (typeof window !== 'undefined' && window.console) {
+                console.log('Yandex.Metrika initialized for counter 103567092');
+              }
             `
           }}
         />
